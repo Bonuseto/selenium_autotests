bb45c0b0b0b9a6937d263123cc9a10cf7072509e
Yevgeniy Fomin <yevgeniy.fomin@buhlergroup.com>
2023-09-18 10:47:18 +0200

    publish

---
+++ POM/testCases/LinksTest.py
@@ -0,0 +1,72 @@
+import unittest
+from selenium import webdriver
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from POM.pageObjects.loginPage import LoginPage
+from POM.pageObjects.homePage import HomePage
+
+sys.tracebacklimit = 0
+
+class LinksTest(unittest.TestCase):
+    baseURL = "https://www.saucedemo.com/"
+    standard_user = "standard_user"
+    problem_user= "problem_user"
+    password = "secret_sauce"
+    homeURL = "https://www.saucedemo.com/inventory.html"
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.get(cls.baseURL)
+        lg = LoginPage(cls.driver)
+        lg.setUsername(cls.standard_user)
+        lg.setPassword(cls.password)
+        lg.clickLogin()
+        cls.AddButtonsErrors = []
+        cls.RemoveButtonsErrors = []
+    def test_SocialLinks(self):
+        hp = HomePage(self.driver)
+        expect_links =["https://twitter.com/saucelabs","https://www.facebook.com/saucelabs","https://www.linkedin.com/company/sauce-labs/"]
+        links = hp.getSocialLinks()
+        for link in links:
+            self.assertIn(link.get_attribute("href"),expect_links,"\nLink Error:" + link.get_attribute("href"))
+            print("Test social link: "+expect_links[links.index(link)] + " - DONE")
+        print("Test social links - DONE")
+    def test_AddtoCart_Activate(self):
+        hp = HomePage(self.driver)
+        addButtons = hp.getAddtoCartButtons()
+
+
+        #check if add to cart buttons working or not
+        for button in addButtons:
+            button.click()
+            afterClick =hp.getAddtoCartButtons()
+
+            try:
+                self.assertEqual("Remove", afterClick[addButtons.index(button)].get_attribute("textContent"),
+                                 addButtons.index(button))
+            except AssertionError as e:self.AddButtonsErrors.append(button.get_attribute("name"))
+
+        self.assertEqual([], self.AddButtonsErrors)
+        print("Test press Add to cart button  - DONE")
+        # check if remove buttons working or not
+        addButtons = hp.getAddtoCartButtons()
+        for button in addButtons:
+            button.click()
+            afterClick = hp.getAddtoCartButtons()
+            try:
+                self.assertEqual("Add to cart", afterClick[addButtons.index(button)].get_attribute("textContent"),
+                                 addButtons.index(button))
+            except AssertionError as e:
+                self.RemoveButtonsErrors.append(button.get_attribute("name"))
+
+        self.assertEqual([],self.RemoveButtonsErrors)
+        print("Test press Remove button  - DONE")
+    @classmethod
+    def tearDownClass(cls):
+
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ POM/testCases/LoginTest.py
@@ -0,0 +1,105 @@
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
+import os
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from POM.pageObjects.loginPage import LoginPage
+from POM.pageObjects.homePage import HomePage
+
+sys.tracebacklimit = 0
+
+"""
+Test login by many types of accounts:
+1: standard account
+2: invalid account
+3: block account
+4: perfomance glitch account (postpone)
+5: problem account
+"""
+
+
+class LoginTest(unittest.TestCase):
+    # Variables
+    baseURL = "https://www.saucedemo.com/"
+    standard_user = "standard_user"
+    invalid_username_user = "invalid_username_user"
+    locked_out_user = "locked_out_user"
+    problem_user = "problem_user"
+    performance_glitch_user = "performance_glitch_user"
+    password = "secret_sauce"
+
+    '''
+    Pre testing: Setting webdriver for Chrome.
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(2)
+
+    '''
+    Step 1: Go to URL.
+    Step 2: Enter username: standard_user.
+    Step 3: Enter password: secret_password.
+    Step 4: Click on button Login.
+    Step 5: verify url.
+    Step 6: Click on Menu at the left side.
+    Step 7: Click on button Logout
+    '''
+
+    def test_login_with_standard_user(self):
+        self.driver.get(self.baseURL)
+        lg = LoginPage(self.driver)
+        lg.setUsername(self.standard_user)
+        lg.setPassword(self.password)
+        lg.clickLogin()
+        act_url = self.driver.current_url
+        exp_url = "https://www.saucedemo.com/inventory.html"
+        self.assertEqual(exp_url, act_url)
+        hp = HomePage(self.driver)
+        hp.clickHamburger()
+        hp.clickLogout()
+        print("Login test with standard_user - DONE")
+    '''
+    Step 1: Go to URL.
+    Step 2: Enter username: locked_out_account.
+    Step 3: Enter password: secret_password.
+    Step 4: Click on button Login.
+    Step 5: verify error message.
+    '''
+
+    def test_login_with_locked_out_user(self):
+        self.driver.get(self.baseURL)
+        lg = LoginPage(self.driver)
+        lg.setUsername(self.locked_out_user)
+        lg.setPassword(self.password)
+        lg.clickLogin()
+        act_error = lg.getErrorInvaldAccount()
+        exp_error = "Epic sadface: Sorry, this user has been locked out."
+        self.assertEqual(exp_error, act_error)
+        print("Login test with locked_out_user - DONE")
+    '''
+       Step 1: Go to URL.
+       Step 2: Enter username: invalid_username_user.
+       Step 3: Enter password: secret_password.
+       Step 4: Click on button Login.
+       Step 5: verify error message.
+       '''
+    def test_login_with_invalid_username_user(self):
+        self.driver.get(self.baseURL)
+        lg = LoginPage(self.driver)
+        lg.setUsername(self.invalid_username_user)
+        lg.setPassword(self.password)
+        lg.clickLogin()
+        act_error = lg.getErrorInvaldAccount()
+        exp_error = "Epic sadface: Username and password do not match any user in this service"
+        self.assertEqual(exp_error, act_error)
+        print("Login test with invalid_username_user - DONE")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
